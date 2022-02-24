20bd1e7a85a55875ca7d093d47da558ecbe9f715
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-02-24 00:30:10 -0300

    Configurando as classes Funcionario, Cargo e ValidacaoExceptio

---
+++ src/model/ValidacaoException.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ValidacaoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidacaoException(String msg){
+		super(msg);
+	}
+	
+}
