c0bc5ac71aee9310228bdf9c2ecf7643a8b6399e
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-02-24 00:53:21 -0300

    Adicionando uma nova validação para reajuste salarial

--- src/service/ReajusteService.java
+++ src/service/ReajusteService.java
@@ -2,6 +2,8 @@ package service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import model.Funcionario;
 import model.ValidacaoException;
@@ -11,8 +13,16 @@ public class ReajusteService {
 	public void reajustarSalarioFuncionario(Funcionario funcionario, BigDecimal aumento) {
 		BigDecimal salarioAtual = funcionario.getSalario();
 		BigDecimal percetualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+		
 		if(percetualReajuste.compareTo(new BigDecimal("0.4")) > 0)
-			throw new ValidacaoException("Reajuste não pode ser superior a 40% do salário atual");
+			throw new ValidacaoException("Reajuste não autorizado!! O aumento não pode ser superior a 40% do salário atual");
+		
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		Long mesesUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+	
+		if(mesesUltimoReajuste < 6)
+			throw new ValidacaoException("Reajuste não autorizado!! O intervalo entre reajustes deve ser de no mínimo 6 meses");
 		
 		BigDecimal salarioReajustado = salarioAtual.add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
