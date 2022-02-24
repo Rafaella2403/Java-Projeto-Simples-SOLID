cad8e37e678fd7944cab41b4abc751e7248a7400
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-02-24 01:16:48 -0300

    Aplicando o princípio do aberto fechado na classe Reajuste

--- src/service/ReajusteService.java
+++ src/service/ReajusteService.java
@@ -1,30 +1,22 @@
 package service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-
+import java.util.List;
 import model.Funcionario;
-import model.ValidacaoException;
 
 public class ReajusteService {
 
-	public void reajustarSalarioFuncionario(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
-		BigDecimal percetualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-		
-		if(percetualReajuste.compareTo(new BigDecimal("0.4")) > 0)
-			throw new ValidacaoException("Reajuste não autorizado!! O aumento não pode ser superior a 40% do salário atual");
-		
-		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
-		LocalDate dataAtual = LocalDate.now();
-		Long mesesUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+	private List<ValidacaoReajuste> listaValidacoesReajuste;
 	
-		if(mesesUltimoReajuste < 6)
-			throw new ValidacaoException("Reajuste não autorizado!! O intervalo entre reajustes deve ser de no mínimo 6 meses");
+	public ReajusteService(List<ValidacaoReajuste> listaValidacoesReajuste) {
+		this.listaValidacoesReajuste = listaValidacoesReajuste;
+	}
+
+	public void reajustarSalarioFuncionario(Funcionario funcionario, BigDecimal aumento) {
 		
-		BigDecimal salarioReajustado = salarioAtual.add(aumento);
+		this.listaValidacoesReajuste.forEach(validacao -> validacao.validar(funcionario, aumento));
+		
+		BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
 	}
 	
